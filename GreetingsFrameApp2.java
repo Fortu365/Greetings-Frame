247d2b641661685906e57584e4135e3813e0f1cd
Fortu365 <fortunatemahlodi46@gmail.com>
2024-08-10 04:58:08 +0200

    Add files via upload

---
+++ GreetingsFrameApp2.java
@@ -0,0 +1,21 @@
+
+package greetingsframeapp2;
+
+import za.ac.tut.gui.GreetingsFrame;
+
+/**
+ *
+ * @author Fortunate
+ */
+public class GreetingsFrameApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Instantiate
+        GreetingsFrame frame = new GreetingsFrame();
+    }
+    
+}
